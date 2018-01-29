548e8363dd5af95acabc86a584862e1425e8987a
Link <linkdoyle@163.com>
2018-01-29 23:28:59 +0800

    add ToString

--- src/core/string.hpp
+++ src/core/string.hpp
@@ -5,7 +5,18 @@
 namespace yuki {
 #ifdef UNICODE
 using String = std::wstring;
+
+template <typename T>
+String ToString(T&& value) {
+  return std::to_wstring(std::forward<T>(value));
+}
 #else
 using String = std::string;
+
+template<typename T>
+String ToString(T&& value) {
+  return std::to_string(std::forward<T>(value));
+}
 #endif
+
 } // namespace yuki
