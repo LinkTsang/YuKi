c1b40fae66e664ae8afd3c497dfb6703281734e6
Link <linkdoyle@163.com>
2018-11-18 17:14:06 +0800

    Add some bat scripts for antlr4.

---
+++ tools/antlr4_javac.bat
@@ -0,0 +1,1 @@
+javac -classpath .;%~dp0\..\thirdparty\antlr\antlr-4.7.1-complete.jar;%CLASSPATH% %*
---
+++ tools/grun.bat
@@ -0,0 +1,1 @@
+java -cp %~dp0\..\thirdparty\antlr\antlr-4.7.1-complete.jar;%CLASSPATH% org.antlr.v4.gui.TestRig %*
